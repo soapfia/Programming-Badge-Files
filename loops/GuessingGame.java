071f93dca99667cd77a106153627a83ac94d259f
Sophia <sophiezCooki2010@gmail.com>
2026-09-04 17:27:29 -0400

    completed guessinggame and started on multtable

--- loops/GuessingGame.java
+++ loops/GuessingGame.java
@@ -15,24 +15,39 @@ public class GuessingGame {
             // 1. Create a variable containing the secret number.
             // ---- BEGIN CODE ----
 
-
+            int randomNum = (int) (Math.random() * 101); // 0-100, exlcuding 101 (less than 101, not equal to)
+            int secret = 5;
+             
 
             // ---- END CODE ----
-
 
             // 2. The code below asks the user for a number between 1 and 100 and converts that guess to a number.
             // Annotate it with comments. 
 
-            Scanner input = new Scanner(System.in);  
-            System.out.println("Enter guess (1-100):");
-
+            Scanner input = new Scanner(System.in);  // creates a scanner input
+            System.out.println("Enter guess (1-100):"); // prints out this statement for the person to guess a # from 1-100
+            
             String guessStr = input.nextLine();
-            int guess = Integer.parseInt(guessStr);      
+            int guess = Integer.parseInt(guessStr);
 
-
+            while (randomNum != guess) {
 
             // 3. Write an if statement that tells the player whether their guess is too high or too low.
             // ---- BEGIN CODE ----
+
+                if (guess < randomNum) {
+                System.out.println("Too low! Try guessing higher next time!");
+                } else if (guess > randomNum) {
+                System.out.println("Too high! Try guessing lower next time!");
+                } 
+                System.out.println("Enter guess (1-100):"); // prints out this statement for the person to guess a # from 1-100
+                guessStr = input.nextLine();
+                guess = Integer.parseInt(guessStr);
+            }
+
+            System.out.println("Congratulations, you guessed it correct!"); 
+
+            }
 
 
 
@@ -54,4 +69,3 @@ public class GuessingGame {
         // 7. Now make the variable an actual secret by randomly generating the number defined in step 1. 
         // Hint: use Java Random!
     }
-}
--- loops/MultTable.java
+++ loops/MultTable.java
@@ -11,17 +11,40 @@ public class MultTable {
         // 1. We will begin by choosing the first number to multiply. Create a variable with a number from 1 to 10. 
         // ---- BEGIN CODE ----
 
+        ////int randomNum = (int) (Math.random() * 11);
+        int num = 7;
+
         // ---- END CODE ----
 
 
         // 2. Print the variable doubled (ie. "__ x 2 = __") 
         // ---- BEGIN CODE ----
 
+        // int result2 = num * 2;
+
+
+
+
         // ---- END CODE ----
 
 
         // 3. Now do this for tripled, quadrupled, so on, until "__ x 10 = __"
         // ---- BEGIN CODE ----
+
+        // int result3 = num * 3;
+        // int result4 = num * 4;
+        // int result5 = num * 5;
+        // int result6 = num * 6;
+        // int result7 = num * 7;
+        // int result8 = num * 8;
+        // int result9 = num * 9;
+        // int result10 = num * 10;
+
+        // System.out.println(result2 + "\n" + result3 + "\n" + result4 + "\n" + result5 + "\n" + result6 + "\n" + result7 + "\n" + result8 + "\n" + result9 + "\n" + result10);
+
+        for (int i = 1; i <= 10; i++) {
+            System.out.println();
+        }
 
         // ---- END CODE ----
 
